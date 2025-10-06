3575d282135d986a64e926c4ebad4873323ceabf
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-10-06 20:50:04 +0000

    Updating to latest FlutterFlow output.

--- lib/onboarding/onboarding_widget.dart
+++ lib/onboarding/onboarding_widget.dart
@@ -38,6 +38,8 @@ class _OnboardingWidgetState extends State<OnboardingWidget> {
 
     _model.textController2 ??= TextEditingController();
     _model.textFieldFocusNode2 ??= FocusNode();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) => safeSetState(() {}));
   }
 
   @override
